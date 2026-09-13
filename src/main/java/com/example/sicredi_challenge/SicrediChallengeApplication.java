7e94d326be4268470615f15cad16e9cfb602fe27
Eduardo Vieira <edusvieirap@gmail.com>
2026-09-12 21:05:19 -0300

    Build: Initialize codebase

---
+++ src/main/java/com/example/sicredi_challenge/SicrediChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sicredi_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicrediChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicrediChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sicredi_challenge/SicrediChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sicredi_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
